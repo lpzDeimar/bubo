96713b5f603496bb93563aaad4d379c075c58400
Deimar Lopez <lpzdeimar@gmail.com>
2025-05-17 19:27:53 -0500

    create web

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        react()
+    ],
+    // Configurar para renderizado en servidor
+    output: 'server',
+    
+    // Configuración para asegurar que las variables de entorno estén disponibles
+    vite: {
+      envPrefix: ['GOOGLE_'],
+      define: {
+        'import.meta.env.GOOGLE_SERVICE_ACCOUNT_EMAIL': 
+          JSON.stringify(process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL),
+        'import.meta.env.GOOGLE_PRIVATE_KEY': 
+          JSON.stringify(process.env.GOOGLE_PRIVATE_KEY)
+      }
+    }
+});
