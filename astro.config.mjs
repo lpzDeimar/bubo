6d1a2c3f4a29541b3879e2af0d0d7b5df0653dc9
Deimar Lopez <lpzdeimar@gmail.com>
2025-08-15 21:33:24 -0500

    New UI and seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: netlify(),
     site: 'https://bubo.plus',
+    output: 'static',
 });
